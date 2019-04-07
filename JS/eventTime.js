9e147dbf9321110944da39ec84f1fd8ae04f18bf
genarogg <gena_gonzalez15@hotmail.com>
2019-04-06 23:25:30 -0400

    JS - eventTime

---
+++ JS/eventTime.js
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Eventos time</title>
+    <link rel="stylesheet" href="">
+</head>
+<body>
+    <script>
+        function saludo(){
+            alert("hola mundo");
+        }
+
+        setTimeout(saludo, 1000);
+        setInterval(saludo, 5000);
+    </script>
+</body>
+</html>
